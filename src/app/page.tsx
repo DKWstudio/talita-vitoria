a729671de436711192cb633c84a82cc9f10b7e46
Daniel Felipe <sapoinfoshop@gmail.com>
2026-08-12 21:54:52 -0300

    Atualiza vitrine apos edicao de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Storefront from "@/components/store/Storefront";
 import { getCatalogProducts } from "@/lib/products";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return <Storefront products={await getCatalogProducts()} />;
 }
